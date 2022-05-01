39b197d59134267c517cf494310c0d8ef1aba538
JymPatel <jympatel@yahoo.com>
2022-05-02 00:02:31 +0530

    pps - determinetype

---
+++ PPS/11_determinetype.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main()
+{
+	char c;
+	printf("enter char: ");
+	scanf("%c", &c);
+	if (c >= 97 && c <= 122)
+	{
+		printf("small character\n");
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		printf("capital\n");
+	}
+	else if (c >= 48 && c <= 57)
+	{
+		printf("digit\n");
+	}
+	else
+	{
+		printf("special character\n");
+	}
+}
