65d3f5dc620f2fea407de0b2904f6958b9a7d6dd
JymPatel <jympatel@yahoo.com>
2022-05-01 23:53:04 +0530

    pps - 10 - maxof3

---
+++ PPS/10_maxof3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	// get integers
+	int a, b, c;
+	printf("a? ");
+	scanf("%i", &a);
+	printf("b? ");
+	scanf("%i", &b);
+	printf("c? ");
+	scanf("%i", &c);
+	// check for greatest
+	if (a > c && a > b)
+	{
+		printf("%i is greatest number", a);
+	}
+	else if (b > c)
+	{
+		printf("%i is greatest number", b);
+	}
+	else
+	{
+		printf("%i is greatest number", c);
+	}
+	printf("\n");
+}
