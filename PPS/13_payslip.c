2474209426c3d6e8c45f44aa576439ac36506716
JymPatel <jympatel@yahoo.com>
2022-05-04 21:17:16 +0530

    pps - payslip

---
+++ PPS/13_payslip.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int basic;
+    printf("enter basic: ");
+    scanf("%d", &basic);
+    // all things
+    float da = basic * 0.10;
+    float hra = basic * 0.075;
+    float ma = 300;
+    float pf = basic * 0.125;
+    float gross = basic + da + hra + ma;
+    float nt = gross - pf;
+    // print
+    printf("BASIC: %i\nDA   : %.*f\nHRA  : %.*f\nMA   : %.*f\nPROFIT %.*f\nGROSS: %.*f\nNET  : %.*f\n", basic, da, 3, hra, 3, ma, 3, pf, 3, gross, 3, nt, 3);
+}
