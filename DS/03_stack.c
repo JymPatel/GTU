6d3f4db7896abcd9976f95e94f530668a509876a
JymPatel <jympatel@yahoo.com>
2022-09-29 12:39:46 +0530

    stack - push, display

--- DS/03_stack.c
+++ DS/03_stack.c
@@ -6,28 +6,55 @@ struct stack{
 };
 
 int pop(struct stack *s){
-
     if (s->top == -1){
         printf("stack empty!\n");
-        return 0;
+        return -1;
     }
 
     int element = (*s).array[(*s).top];
     (*s).array[(*s).top] = 0;
-    (*s).top -= 1;
+    (*s).top--;
 
     return element;
 }
 
-int 
+
+int push(struct stack *s, int element){
+    if (s->top == 99){
+        printf("stack full, element not added!\n");
+        return 1;
+    }
+
+    (*s).top++;
+    (*s).array[(*s).top] = element;
+    return 0;
+}
+
+
+int display(struct stack *s){
+    if (s->top == -1){
+        printf("stack empty!\n");
+        return -1;
+    }
+
+    printf("%d\n", s->array[(*s).top]);
+    return s->array[(*s).top];
+}
+
 
 int main(){
     struct stack ss;
     ss.top = 0;
     ss.array[0] = 5;
-    printf("%d %d %d\n", pop(&ss), ss.top, ss.array[0]);
-    printf("%d %d", ss.top, ss.array[0]);\
-        printf("%d %d %d\n", pop(&ss), ss.top, ss.array[0]);
-
-    return(0);
+    display(&ss);
+    pop(&ss);
+    display(&ss);
+    push(&ss, 8);
+    display(&ss);
+    for(int i = 0; i < 111; i++){
+        push(&ss, i);
+        display(&ss);
+    }
+    
+    return 0;
 }
