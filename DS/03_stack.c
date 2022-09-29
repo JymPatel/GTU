5421b00fdcd63adb7b54ec4acb7754eab078e663
JymPatel <jympatel@yahoo.com>
2022-09-29 12:23:16 +0530

    stack - pop

--- DS/03_stack.c
+++ DS/03_stack.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
+struct stack{
+    int top;
+    int array[100];
+};
+
+int pop(struct stack *s){
+
+    if (s->top == -1){
+        printf("stack empty!\n");
+        return 0;
+    }
+
+    int element = (*s).array[(*s).top];
+    (*s).array[(*s).top] = 0;
+    (*s).top -= 1;
+
+    return element;
+}
+
+int 
+
 int main(){
-	return(0);
+    struct stack ss;
+    ss.top = 0;
+    ss.array[0] = 5;
+    printf("%d %d %d\n", pop(&ss), ss.top, ss.array[0]);
+    printf("%d %d", ss.top, ss.array[0]);\
+        printf("%d %d %d\n", pop(&ss), ss.top, ss.array[0]);
+
+    return(0);
 }
