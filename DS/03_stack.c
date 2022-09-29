65bb648e6ae5e066f545b95c94c6d1732e2a1615
JymPatel <jympatel@yahoo.com>
2022-09-29 12:57:49 +0530

    stack - peek, change

--- DS/03_stack.c
+++ DS/03_stack.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-struct stack{
+struct Stack{
     int top;
     int array[100];
 };
 
-int pop(struct stack *s){
+int pop(struct Stack *s){
     if (s->top == -1){
         printf("stack empty!\n");
         return -1;
@@ -18,8 +18,7 @@ int pop(struct stack *s){
     return element;
 }
 
-
-int push(struct stack *s, int element){
+int push(struct Stack *s, int element){
     if (s->top == 99){
         printf("stack full, element not added!\n");
         return 1;
@@ -30,31 +29,60 @@ int push(struct stack *s, int element){
     return 0;
 }
 
+int change(struct Stack *s, int index, int element){
+    if (s->top < index || index < 0){
+        printf("no element at that index\n");
+        return 1;
+    }
+    
+    s->array[index] = element;
+    return 0;
+}
 
-int display(struct stack *s){
-    if (s->top == -1){
-        printf("stack empty!\n");
+int peek(struct Stack *s){
+    if (s->top < 0){
         return -1;
     }
-
-    printf("%d\n", s->array[(*s).top]);
     return s->array[(*s).top];
 }
 
+int display(struct Stack *s){
+    if (s->top == -1){
+        printf("stack empty!\n");
+        return 1;
+    }
+
+    printf("%d\n", s->array[(*s).top]);
+    return 0;
+}
 
 int main(){
-    struct stack ss;
-    ss.top = 0;
-    ss.array[0] = 5;
-    display(&ss);
-    pop(&ss);
-    display(&ss);
-    push(&ss, 8);
-    display(&ss);
-    for(int i = 0; i < 111; i++){
-        push(&ss, i);
-        display(&ss);
+    struct Stack myStack;
+    myStack.top = 0;
+    myStack.array[0] = 5;
+    display(&myStack);
+    pop(&myStack);
+    display(&myStack);
+    push(&myStack, 8);
+    display(&myStack);
+    pop(&myStack);
+    display(&myStack);
+
+    pop(&myStack);
+    display(&myStack);
+    pop(&myStack);
+    display(&myStack);
+
+    for (int i = 0; i < 95; i++){
+        push(&myStack, i);
     }
-    
+    for (int i = 0; i < 10; i++){
+        push(&myStack, i);
+        display(&myStack);
+    }
+    printf("%d\n", peek(&myStack));
+    printf("%d\n", peek(&myStack));
+    printf("%d\n", peek(&myStack));
+
     return 0;
 }
