3695dcece7d8dbecaeab9e310c0b7979b7bd70a0
jayam04 <93824505+jayam04@users.noreply.github.com>
2023-04-18 20:01:59 +0530

    added new classes to tictactoe

--- playgrounds/tic-tac-toe.py
+++ playgrounds/tic-tac-toe.py
@@ -1,6 +1,10 @@
 class TicTacToe:
-    def __init__(self):
+    def __init__(self, player1, player2):
         self.arr = [str(i + 1) for i in range(9)]
+        self.player1 = player1
+        self.player2 = player2
+        self.winner = None
+        self.moves = []
 
     def __str__(self) -> str:
         string = ""
@@ -22,7 +26,22 @@ class TicTacToe:
             
     def check_win(self):
         diff_char = set([i for i in self.arr if i < '1' or i > '9'])
-        
+
+
+class Player:
+    def __init__(self, name, score=0):
+        self.name = name
+        self.score = score
+
+
+class Games:
+    def __init__(self, players, games_to_play=[]):
+        self.played = []
+        self.to_play = games_to_play
+        self.players = players
+
+
+players = Player()
 tictactoe = TicTacToe()
 tictactoe.play('o', 1)
 tictactoe.check_win()
