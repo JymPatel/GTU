49b876d8aadc65bb816697cfce5f93ddd8a91d11
jayam04 <93824505+jayam04@users.noreply.github.com>
2023-04-25 12:59:46 +0530

    updated tic-tac-toe

--- playgrounds/tic-tac-toe.py
+++ playgrounds/tic-tac-toe.py
@@ -1,37 +1,52 @@
+from termcolor import cprint, colored 0
+
 class TicTacToe:
     def __init__(self, player1, player2):
         self.arr = [str(i + 1) for i in range(9)]
         self.player1 = player1
         self.player2 = player2
+        self.currentTurn = player1
         self.winner = None
         self.moves = []
+
 
     def __str__(self) -> str:
         string = ""
         for i in range(9):
             if i % 3 == 0 and i != 0:
                 string += '\n'
-                string += '-----------\n'
+                string += '---+---+---\n'
             elif i != 0:
-                string += '|'
+                string += '┆'
             
             string += f' {self.arr[i]} '
         return string
     
-    def play(self, char, place):
-        if char >= '1' and char <= '9':
-            raise ValueError
+    
+    def play(self, place):
+        if self.arr[place - 1] < '1' or self.arr[place - 1] > '9':
+            raise ValueError('incorrect move!')   
+        self.arr[place - 1] = self.currentTurn.char
+        self.moves.append(place - 1)
+        self.change_turn()
+            
+            
+    def change_turn(self):
+        if self.currentTurn == self.player1:
+            self.currentTurn = self.player2
         else:
-            self.arr[place - 1] = char
+            self.currentTurn = self.player1
+            
             
     def check_win(self):
-        diff_char = set([i for i in self.arr if i < '1' or i > '9'])
+        pass
 
 
 class Player:
-    def __init__(self, name, score=0):
+    def __init__(self, name, char, score=0):
         self.name = name
         self.score = score
+        self.char = char
 
 
 class Games:
@@ -39,10 +54,22 @@ class Games:
         self.played = []
         self.to_play = games_to_play
         self.players = players
+        
+    def create_games(self):
+        pass
 
 
-players = Player()
-tictactoe = TicTacToe()
-tictactoe.play('o', 1)
+player1 = Player('jayam', 'X')
+player2 = Player('pratham', 'O')
+tictactoe = TicTacToe(player1, player2)
+# tictactoe.play(1)
+# tictactoe.play(3)
 tictactoe.check_win()
 print(tictactoe)
+
+
+new_tictac = TicTacToe(player2, player1)
+for i in range(9):
+    new_tictac.play(int(input("enter number: ")))
+    print(new_tictac)
+    
