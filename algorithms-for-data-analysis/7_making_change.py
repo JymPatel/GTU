28c04ab25b6d5d95ff1974f9f031107cd8f7d3b1
Jayam <jayampatel04@gmail.com>
2023-10-25 23:47:27 +0530

    ada/ completed coinchange

--- algorithms-for-data-analysis/7_making_change.py
+++ algorithms-for-data-analysis/7_making_change.py
@@ -1,17 +1,30 @@
 from pprint import pprint
 
 coins = [1, 2, 4, 5]
-max_change = 7
-knap_arr = [[-1 for i in range(max_change + 1)] for _ in range(len(coins) + 1)]
+max_change = 8
+knap_arr = [[0 for i in range(max_change + 1)] for _ in range(len(coins) + 1)]
 
 def populate_dp():
-    for i in range(len(coins) + 1):
-        for j in range(max_change + 1):
-            if i == 0 or j == 0:
-                knap_arr[i][j] = 0
-            else:
-                pass
-        print(knap_arr)
+    for i in range(max_change + 1):
+        knap_arr[0][i] = 0
+    for i in range(1, len(coins) + 1):
+        for j in range(coins[i - 1]):
+            knap_arr[i][j] = knap_arr[i - 1][j]
+        for j in range(coins[i - 1], max_change + 1):
+            knap_arr[i][j] = 1 + knap_arr[i][j - coins[i - 1]]
+
+def get_change(change, max_coin = len(coins)):
+    print(f"({change}, {coins[max_coin - 1]})")
+    if change <= 0:
+        return []
+    if knap_arr[max_coin][change] == knap_arr[max_coin - 1][change]:
+        return get_change(change, max_coin - 1)
+    else:
+        c = get_change(change - coins[max_coin - 1], max_coin)
+        c.append(coins[max_coin - 1])
+        return c
+
 
 populate_dp()
 pprint(knap_arr)
+pprint(get_change(8))
--- algorithms-for-data-analysis/todo.md
+++ algorithms-for-data-analysis/todo.md
@@ -7,11 +7,11 @@
   - [x] d. merge sort
   - [ ] e. quick sort
 - [x] 2. Time Analysis of linear and binary search
-- [x] 3.
+- [x] 3. Heap Sort
 - [x] 4. Time Analysis b/w iterative and recursive functions.
 - [x] 5. Knapsack 01 (Dynamic Approach)
-- [ ] 6.
-- [ ] 7.
+- [ ] 6. Chain Matrix using DP
+- [x] 7. Coin Change using DP
 - [x] 8. Knapsack (Greedy Approach)
 - [x] 9. Graph and Searching (BFS, DFS)
 - [x] 10. Prim's Algorithm
