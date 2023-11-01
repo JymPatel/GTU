28821d60dc416bf4d03abf9cbf99991bdeea2cfd
Jayam <jayampatel04@gmail.com>
2023-11-01 10:22:47 +0530

    updated quick sort to print all steps

--- algorithms-for-data-analysis/1_quick_sort.py
+++ algorithms-for-data-analysis/1_quick_sort.py
@@ -1,15 +1,20 @@
 import time
 
 def quick_sort(arr):
+    print(arr)
     if len(arr) <= 1:
         return arr
     else:
         pivot = arr[0]
         less_than_pivot = [x for x in arr[1:] if x <= pivot]
         greater_than_pivot = [x for x in arr[1:] if x > pivot]
-        return quick_sort(less_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
+        sorted_arr = quick_sort(less_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
+
+        print(sorted_arr)
+        return sorted_arr
 
 my_list = [3, 6, 8, 10, 1, 2, 1]
+print(f"Initial Array: {my_list}")
 t1 = time.time()
 sorted_list = quick_sort(my_list)
 print(f"Sorted Array: {sorted_list}")
