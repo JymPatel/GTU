93b42dc901f3f3770d00fdc880cb2155461b81a4
jayam04 <jayampatel04@gmail.com>
2024-02-07 09:11:32 +0530

    wp/ new changes

--- advanced-java-programming/Practical02/Practical02.java
+++ advanced-java-programming/Practical02/Practical02.java
@@ -2,23 +2,32 @@ package Practical02;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Practical02 {
     public static void main(String[] args) {
+        String query = "INSERT INTO comp31 VALUES (1, 2, 3)";
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306",
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/students",
                     "root", "");
 
             Statement statement = con.createStatement();
-            String query = "CREATE DATABASE diet";
 
-            statement.execute(query);
             // int i = statement.executeUpdate(query);
-            // System.out.println("total no. of rows updated=" + i);
+            // System.out.println("total no. of rows updated = " + i);
 
-            statement.close();
+            int a = statement.executeUpdate(query);
+            // statement.executeQuery(query);
+
+                // System.out.println(resultSet);
+                // System.out.println(resultSet.getString(1));
+                // while (resultSet.next()) {
+                //     System.out.println("hey");
+                //     System.out.println(resultSet.getInt(5));
+                // }
+            // statement.close();
             con.close();
         } catch (Exception exception) {
             exception.printStackTrace();
